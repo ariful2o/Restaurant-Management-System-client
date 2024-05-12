d44144c52fe783037181ff2e0c98688ed4a34adf
mohammadariful20 <arifulz848@gmail.com>
2024-05-12 13:44:35 +0600

    Navbar design theme change show profile

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../footer/Footer";
+import Navbar from "../pages/home/share/Navbar";
 
 
 export default function Root() {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     )
 }
--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,14 +1,40 @@
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 import PropTypes from 'prop-types';
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import auth from "../firebase/firebase.init";
 
 export const AuthContext = createContext(null)
 export default function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
 
+    const registerUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+    const loginUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser.user) {
+                setUser(currentUser.user)
+                setLoading(false)
+                console.log(currentUser.user)
+            }
+        })
+        return ()=>unsubscribe()
+    }, [])
+
+
+
     const authInfo = {
         user,
-        loading
+        loading,
+        registerUser,
+        loginUser,
     }
     return (
         <AuthContext.Provider value={authInfo}>
