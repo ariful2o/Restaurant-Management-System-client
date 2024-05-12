367d0357d487947a709317bb8151fb263900a6c7
mohammadariful20 <arifulz848@gmail.com>
2024-05-12 11:17:30 +0600

    React-Loading-Skeleton package add

--- src/private/PrivateRoute.jsx
+++ src/private/PrivateRoute.jsx
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types';
 import { Navigate } from 'react-router-dom';
 import { AuthContext } from '../provider/AuthProvider';
 import { useContext } from 'react';
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function PrivateRoute({children}) {
     const {user, loading} = useContext(AuthContext)
 
     if (loading) {
-        return <span className="loading loading-spinner loading-lg"></span>
+        return <Skeleton count={10} />
     }
     if (user) {
         return children;
